3623c2b82a7bd526e8d8254ebc68e21dd6ff3728
apurba4u <apurba185loser@gmail.com>
2026-04-13 23:04:17 +0600

    Start The Journey

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,11 +18,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html
+    <html data-theme="light"
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="max-w-7xl mx-auto text-center border">{children}</body>
     </html>
   );
 }
